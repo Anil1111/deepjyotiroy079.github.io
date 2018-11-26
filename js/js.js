c95033ac111dbf03273e3ff5c861f7b3d250b05c
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2018-11-26 21:38:45 +0530

    added about section

--- js/js.js
+++ js/js.js
@@ -7,5 +7,7 @@ $(document).ready(function(){
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
         $('nav').addClass('white');
+    } else {
+        $('nav').removeClass('white');
     }
 });
