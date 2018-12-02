ec590697011a9480be42ffc2bf3e6652e27f6ead
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2018-12-02 21:09:39 +0530

    pre loader

--- js/js.js
+++ js/js.js
@@ -1,3 +1,11 @@
+$(document).ready(function($) {  
+
+	// site preloader -- also uncomment the div in the header and the css style for #preloader
+	$(window).load(function(){
+		$('#preloader').fadeOut('slow',function(){$(this).remove();});
+	});
+	
+});
 $(document).ready(function(){
     $(".menu-icon").on("click", function() {
         $("nav ul").toggleClass("showing");
