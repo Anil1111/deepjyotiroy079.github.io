eb577eb7e5e38ec314887f30cabe9cf5c153a44b
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2018-12-04 22:14:37 +0530

    minor changes

--- js/scroll.js
+++ js/scroll.js
@@ -19,7 +19,7 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 900, function() {
+        }, 1000, function() {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
